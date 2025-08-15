a1082bfd6a7bfba7b6d370a9f71c26ba5eaeb08a
Vitor Machado Neves <vitormachado743@gmail.com>
2025-08-15 02:08:35 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/landing/interest-section.tsx
+++ src/components/landing/interest-section.tsx
@@ -18,6 +18,7 @@ const testimonials = [
     avatar: 'LM',
     stars: 5,
     text: 'Eu estava completamente estagnado nos 700 pontos em matemática. O método de focar nos 20% de conteúdo que mais caem e a prática ativa simplesmente viraram o jogo. Saí de 400 para 850. o método é incrível.',
+    image: 'https://i.imgur.com/0bC4Hzq.png',
   },
   {
     name: 'Mariana A.',
