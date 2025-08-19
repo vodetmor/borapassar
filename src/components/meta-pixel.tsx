db5d1a36d4c36411bb6d4bb733e67bb21cd5b386
Vitor Machado Neves <vitormachado743@gmail.com>
2025-08-19 01:34:44 +0000

    nada está funcionando me fale as pastas onde está localizado o html prin

--- src/components/meta-pixel.tsx
+++ src/components/meta-pixel.tsx
@@ -1,19 +1,53 @@
-'use client'
+"use client";
 
-import { usePathname, useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
+import { usePathname, useSearchParams } from "next/navigation";
+import Script from "next/script";
+import { useEffect, useState } from "react";
 
 export function MetaPixel() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [pixelLoaded, setPixelLoaded] = useState(false);
 
   useEffect(() => {
-    // The initial 'PageView' is fired in the script in layout.tsx.
-    // This effect is responsible for tracking subsequent page views in the SPA.
-    if (typeof window !== 'undefined' && (window as any).fbq) {
-      (window as any).fbq('track', 'PageView');
-    }
-  }, [pathname, searchParams])
+    if (!pixelLoaded) return;
 
-  return null
+    const url = pathname + searchParams.toString();
+    // Subsequent page views
+    if (window.fbq) {
+        window.fbq('track', 'PageView');
+    }
+  }, [pathname, searchParams, pixelLoaded]);
+
+  return (
+    <>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        onLoad={() => setPixelLoaded(true)}
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2208909466198043');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=2208909466198043&ev=PageView&noscript=1"
+        />
+      </noscript>
+    </>
+  );
 }
--- src/services/meta-api.ts
+++ src/services/meta-api.ts
@@ -1,4 +1,3 @@
-
 'use server';
 
 import { headers } from 'next/headers';
