12e093793d4cc441d9e594531b95574f841fcce3
Vitor Machado Neves <vitormachado743@gmail.com>
2025-08-20 00:19:41 +0000

    {   "task": "Instalar Meta Pixel no projeto React/Next (.tsx)",   "steps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" rel="stylesheet" />
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
+        <script
           dangerouslySetInnerHTML={{
             __html: `
               !function(f,b,e,v,n,t,s)
