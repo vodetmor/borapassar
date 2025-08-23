96defd5b192c6fb2b8148f6d3e406b1f09a1d342
Vitor Machado Neves <vitormachado743@gmail.com>
2025-08-23 19:33:56 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    allowedDevOrigins: [
+        "https://*.cloudworkstations.dev",
+        "https://*.firebase.app"
+    ]
+  },
   images: {
     remotePatterns: [
       {
