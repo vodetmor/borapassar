6f67f1c23e94919aec13a1f99323c8dd0815053f
Vitor Machado Neves <vitormachado743@gmail.com>
2025-08-28 17:41:40 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <MethodologySectio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import { FaqSection } from '@/components/landing/faq-section';
 import { Footer } from '@/components/landing/footer';
 import { Header } from '@/components/landing/header';
 import { InterestSection } from '@/components/landing/interest-section';
-import { MethodologySection } from '@/components/landing/methodology-section';
 import { MindMapsPreviewSection } from '@/components/landing/mindmaps-preview-section';
 import { OfferSection } from '@/components/landing/offer-section';
 import { StickyCtaButton } from '@/components/landing/sticky-cta-button';
@@ -31,7 +30,6 @@ export default function Home() {
         <ComparisonSection />
         <DesireSection />
         <CredibilitySection />
-        <MethodologySection />
         <MindMapsPreviewSection />
         <InterestSection />
         <OfferSection />
