0b738340675ec888f5652e390eaadef8943b1af2
Vitor Machado Neves <vitormachado743@gmail.com>
2025-09-07 01:11:11 +0000

    Apague TODOS os pixels associados ao facebook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { Toaster } from "@/components/ui/toaster"
 import { WhatsAppButton } from '@/components/landing/whatsapp-button';
 import React, { Suspense } from 'react';
 import Script from 'next/script';
-import { MetaPixel } from '@/components/meta-pixel';
 import { SocialProofToast } from '@/components/landing/social-proof-toast';
 
 export const metadata: Metadata = {
@@ -24,20 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" rel="stylesheet" />
-        <Script id="meta-pixel-init" strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '2208909466198043');
-        `}
-      </Script>
-      <Script
+        <Script
           id="utmify-script"
           src="https://cdn.utmify.com.br/scripts/utms/latest.js"
           data-utmify-prevent-xcod-sck
@@ -51,9 +37,6 @@ export default function RootLayout({
         <WhatsAppButton />
         <Toaster/>
         <SocialProofToast />
-        <Suspense fallback={null}>
-            <MetaPixel />
-        </Suspense>
       </body>
     </html>
   );
