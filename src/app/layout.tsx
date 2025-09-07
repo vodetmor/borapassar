f8583558286278ad4e1b90346d7e435c24ddaaab
Vitor Machado Neves <vitormachado743@gmail.com>
2025-09-07 01:24:16 +0000

    remova todos os pixels da utmify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,14 +23,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" rel="stylesheet" />
-        <Script
-          id="utmify-script"
-          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
-          data-utmify-prevent-xcod-sck
-          data-utmify-prevent-subids
-          async
-          defer
-        />
       </head>
       <body className="font-body antialiased">
         {children}
